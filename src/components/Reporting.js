1ec68ea5327b10818e4d56b17c471626b804db7c
thebiggestcookie <eduardo@flipshop.com>
2024-08-19 10:32:14 -0700

    Update Reporting.js

---
+++ src/components/Reporting.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Reporting = () => {
+  return (
+    <div>
+      <h1>Reporting</h1>
+      {/* Implement Reporting functionality here */}
+    </div>
+  );
+};
+
+export default Reporting;
+
