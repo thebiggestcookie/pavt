d92ebb85143a524d5d007a3b19163e0e3bc1452b
thebiggestcookie <eduardo@flipshop.com>
2024-08-19 15:29:41 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,34 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navigation from './components/Navigation';
 import ProductGenerator from './components/ProductGenerator';
 import AttributeEditor from './components/AttributeEditor';
-import AttributeEditorV2 from './components/AttributeEditorV2';
 import HumanGrader from './components/HumanGrader';
 import HumanGraderV2 from './components/HumanGraderV2';
-import Reporting from './components/Reporting';
-import LLMConfigurator from './components/LLMConfigurator';
+import AdminPanel from './components/AdminPanel';
+import PromptManagement from './components/PromptManagement';
+import UserManagement from './components/UserManagement';
+import ReportsDashboard from './components/ReportsDashboard';
+import PromptTester from './components/PromptTester';
+import Login from './components/Login';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/product-generator">Product Generator</Link></li>
-            <li><Link to="/attribute-editor">Attribute Editor</Link></li>
-            <li><Link to="/attribute-editor-v2">Attribute Editor V2</Link></li>
-            <li><Link to="/human-grader">Human Grader</Link></li>
-            <li><Link to="/human-grader-v2">Human Grader V2</Link></li>
-            <li><Link to="/reporting">Reporting</Link></li>
-            <li><Link to="/llm-configurator">LLM Configurator</Link></li>
-          </ul>
-        </nav>
-
+        <Navigation />
         <Routes>
-          <Route path="/" element={<h1>Welcome to the Product Attribute Value Tool</h1>} />
-          <Route path="/product-generator" element={<ProductGenerator />} />
-          <Route path="/attribute-editor" element={<AttributeEditor />} />
-          <Route path="/attribute-editor-v2" element={<AttributeEditorV2 />} />
-          <Route path="/human-grader" element={<HumanGrader />} />
-          <Route path="/human-grader-v2" element={<HumanGraderV2 />} />
-          <Route path="/reporting" element={<Reporting />} />
-          <Route path="/llm-configurator" element={<LLMConfigurator />} />
+          <Route path="/" element={<ProductGenerator />} />
+          <Route path="/grader" element={<HumanGrader />} />
+          <Route path="/grader-v2" element={<HumanGraderV2 />} />
+          <Route path="/admin" element={<AdminPanel />} />
+          <Route path="/prompts" element={<PromptManagement />} />
+          <Route path="/users" element={<UserManagement />} />
+          <Route path="/attributes" element={<AttributeEditor />} />
+          <Route path="/reports" element={<ReportsDashboard />} />
+          <Route path="/prompt-tester" element={<PromptTester />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </Router>
